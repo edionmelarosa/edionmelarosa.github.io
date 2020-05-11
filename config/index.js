de58020b0a740300143a4ae5df8a89811048fd0e
Espiridion Larosa <edion.larosa@gmail.com>
2020-05-11 20:29:30 +0800

    build

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/personal-site',
+        assetsPublicPath: '/personal-site/',
 
         /**
          * Source Maps
