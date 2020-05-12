201738c216910d4bcd65d272215c2165f4668658
Espiridion Larosa <edion.larosa@gmail.com>
2020-05-12 12:27:35 +0800

    build

--- src/components/sections/About.vue
+++ src/components/sections/About.vue
@@ -10,13 +10,13 @@
         <p>An active member/volunteer (acting as Program Head) of <a href="https://devcon.ph/" target="_blank">DevCon Davao</a>, Philippines. Where we share our knowledge and experience with students, educators, professionals and enthusiasts to Sync, Support and Succeed.</p>
         <p>
             Find me on
-        <a class="icon" target="_blank" href="https://github.com/edionmelarosa">
-        <i class="fab fa-github"></i></a>,
-        <a class="icon" target="_blank" href="https://www.linkedin.com/in/espiridion-larosa-2a1316a3/">
-        <i class="fab fa-linkedin"></i></a>
-        <span>and</span>
-        <a class="icon" target="_blank" href="mailto:edion.larosa@email.com">
-        <i class="fas fa-envelope"></i></a>.
+            <a class="icon" target="_blank" href="https://github.com/edionmelarosa">
+            <i class="fab fa-github"></i></a>,
+            <a class="icon" target="_blank" href="https://www.linkedin.com/in/espiridion-larosa-2a1316a3/">
+            <i class="fab fa-linkedin"></i></a>
+            <span>and</span>
+            <a class="icon" target="_blank" href="mailto:edion.larosa@email.com">
+            <i class="fas fa-envelope"></i></a>.
         </p>
     </section>
 </template>
